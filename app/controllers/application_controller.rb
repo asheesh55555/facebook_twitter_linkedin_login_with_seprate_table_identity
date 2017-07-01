1998e51dab0e1b83f944ca1673163ae02c239d62
asheesh55555 <asheeshvits@gmail.com>
2017-07-01 18:36:32 +0530

    hello

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+	before_action :ensure_signup_complete, only: [:new, :create, :update, :destroy]
+	def ensure_signup_complete
+    # Ensure we don't go into an infinite loop
+    return if action_name == 'finish_signup'
+
+    # Redirect to the 'finish_signup' page if the user
+    # email hasn't been verified yet
+    if current_user && !current_user.email_verified?
+      redirect_to finish_signup_path(current_user)
+    end
+  end
+  def after_sign_in_path_for(resource)
+    new_article_path
+  end
+end
