7ed8efb2ef9e6b4e4274d741d1fc4d36f734b183
asheesh55555 <asheeshvits@gmail.com>
2017-07-03 20:05:47 +0530

    aaaaaa

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,16 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
+
+
+
+
+
+//= require jquery
+//= require jquery_ujs
+//= require rails_emoji_picker
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
