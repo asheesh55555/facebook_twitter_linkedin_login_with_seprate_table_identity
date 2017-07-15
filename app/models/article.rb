102f1f07cd6428f69529ec5dda09d600c5a5f058
asheesh55555 <asheeshvits@gmail.com>
2017-07-15 13:33:55 +0530

    gg

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
 	  has_many :taggings
     has_many :users, through: :taggings
 	 has_many :endorses
+	 belongs_to :user
 	  acts_as_votable
 	    mount_uploader :img, AbcUploader
 end
